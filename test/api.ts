5dc151b88bcac47c37f767d4c30abf655607ac5a
ephoton <i@ephoton.me>
2017-10-28 23:37:51 +0800

    Feat: Add unit tests

---
+++ test/api.ts
@@ -0,0 +1,15 @@
+import * as Path from 'path';
+import * as Mocha from 'mocha';
+import * as Chai from 'chai';
+
+const { assert, expect } = Chai;
+const should = Chai.should();
+
+describe('Utils', () => {
+
+  describe('getRouteName', () => {
+    it('should return equals', () => {
+      //
+    });
+  });
+});
